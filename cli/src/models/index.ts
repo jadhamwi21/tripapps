5d8893213d50a3491085043187563542607450e5
Jad <j.hamwi@centroidsol.com>
2023-09-16 19:39:42 +0300

    cli excluded from docker-compose

--- cli/src/models/index.ts
+++ cli/src/models/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
+  console.log(process.env.MONGODB_URL!);
+
   await mongoose.connect(process.env.MONGODB_URL!);
 };
 export const disconnectFromDatabase = async () => {
