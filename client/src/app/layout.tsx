0672d85cec4dd4946a886018b3a37214b145dcf3
Jad <j.hamwi@centroidsol.com>
2023-09-18 11:48:25 +0300

    bugfix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import StyledComponentsRegistry from "@/lib/registry";
 import "./index.css";
 import Header from "@/layouts/Header/Header";
@@ -7,21 +6,21 @@ import React from "react";
 import { AnimatePresence } from "framer-motion";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <StyledComponentsRegistry>
-          <Header />
-          <AnimatePresence mode="wait" initial={false}>
-            {children}
-          </AnimatePresence>
-          {/*<Footer />*/}
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<StyledComponentsRegistry>
+					<Header />
+					<AnimatePresence mode="wait" initial={false}>
+						{children}
+					</AnimatePresence>
+					{/*<Footer />*/}
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }
