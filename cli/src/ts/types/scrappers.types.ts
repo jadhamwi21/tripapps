186d69394c5534a43b6394da0ad266e00b440acd
Jad <j.hamwi@centroidsol.com>
2023-09-29 12:38:40 +0300

    initial commit

--- cli/src/ts/types/scrappers.types.ts
+++
@@ -1,9 +0,0 @@
-type DeepPartial<T> = T extends object ? {
-  [P in keyof T]?: DeepPartial<T[P]>;
-} : T;
-
-export type ScrapeResults = {
-  [country: string]: {
-    [category: string]: { apps: string[], subcategories?: { [subcategory: string]: string[] } }
-  }
-}
--- client/src/constants/constants.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-  black: "#222831",
-  light_grey: "#393E46",
-  yellow: "#FFD369",
-  grey: " #EEEEEE",
-};
