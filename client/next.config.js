b1c33f22ca4b02df07f95d6a9dee9f45fbe7b921
Jad <j.hamwi@centroidsol.com>
2023-10-13 13:00:52 +0300

    domains

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["play-lh.googleusercontent.com", "is1-ssl.mzstatic.com"],
+		domains: ["play-lh.googleusercontent.com", "apps.apple.com"],
 	},
 	typescript: { ignoreBuildErrors: true },
 
