f0599eb7f14860f6d113ca2f507bb2ebfc1fbf1a
Jad <j.hamwi@centroidsol.com>
2023-10-14 11:14:13 +0300

    scrapers update

--- scrapers/engine.py
+++ scrapers/engine.py
@@ -32,16 +32,10 @@ BASE_URL = "https://www.googleapis.com/customsearch/v1"
 
 
 class AppsEngine:
-    def getRelatedLinks(self, links: list[str], site: str):
-        relatedLinks = []
-        for link in links:
-            if link.startswith("https://{}".format(site)):
-                relatedLinks.append(link)
-        return relatedLinks
-
     def getAppsLinks(self, category: str, store: str, location: str):
         site = STORES_SITES[store]
-        query = quote("site:{} {} apps {}".format(site, category, location))
+        query = quote("site:{} {} apps \"{}\"".format(
+            site, category, location))
         response = requests.get('{}?q={}&key={}&cx={}'.format(
             BASE_URL, query, API_KEY, SEARCH_ENGINE_ID))
 
