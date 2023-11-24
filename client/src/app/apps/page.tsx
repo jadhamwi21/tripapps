c0aa9da09b0f66f9cf97e4f49ccf69a89b312ef1
Jad <j.hamwi@centroidsol.com>
2023-11-24 21:50:32 +0300

    homepage cards width fix

--- client/src/app/apps/page.tsx
+++ client/src/app/apps/page.tsx
@@ -12,7 +12,18 @@ const page = async () => {
 	return (
 		<PageWrapper>
 			<FindAppsSearch seeds={seeds} />
-			<AppsList apps={[]} />
+			<AppsList
+				apps={[
+					{
+						name: "DB Navigator",
+						id: "#23",
+						image:
+							"https%3A%2F%2Fplay-lh.googleusercontent.com%2F6ErQQ0bzaLghv2F2dm5smkPjYQ_t9uep0yi3OSYY16O9QdyQfV1Uj2TW5MT-wRHJbg%3Dw240-h480-rw&w=64&q=75",
+						link: "",
+						keywords: [],
+					},
+				]}
+			/>
 		</PageWrapper>
 	);
 };
