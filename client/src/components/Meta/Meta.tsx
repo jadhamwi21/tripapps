574bca073ba37a863a17093a3db3a71dc2add760
Jad <j.hamwi@centroidsol.com>
2023-12-04 17:26:26 +0300

    added icons

--- client/src/components/Meta/Meta.tsx
+++ client/src/components/Meta/Meta.tsx
@@ -6,6 +6,18 @@ const Meta = ({ title, children, description }: Props) => {
 	return (
 		<>
 			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<link
+				href="/apple-touch-icon.png"
+				rel="apple-touch-icon"
+				type="image/png"
+				sizes="144x144"
+			/>
+			<link
+				rel="icon"
+				href="/android-chrome-192x192.png"
+				type="image/png"
+				sizes="192x192"
+			/>
 			<title>{title}</title>
 			{description && <meta name="description" content={description} />}
 
